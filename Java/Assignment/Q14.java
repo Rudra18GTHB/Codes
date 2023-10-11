1ae1665d84879d4f756122fcc9e5c672f01faef2
Rudra Kashyap <rudrak04sh@gmail.com>
2023-10-11 07:36:10 +0530

    Q14

--- Java/Assignment/Q14.java
+++ Java/Assignment/Q14.java
@@ -1,96 +1,186 @@
-class Person{
-    private String mName;
-    private String mBirthDate;
-    private String mSex;
+//Question14 JAVA ASSIGNMENT 1
 
-    public Person(String name, String birthDate, String sex) {
-        this.mName = name;
-        this.mBirthDate = birthDate;
-        this.mSex = sex;
+interface Person {
+    String getName();
+    String getBirthdate();
+    String getSex();
+}
+
+class PersonClass implements Person {
+    public String name;
+    public String birthdate;
+    public String sex;
+
+    public PersonClass(String name, String birthdate, String sex) {
+        this.name = name;
+        this.birthdate = birthdate;
+        this.sex = sex;
     }
 
+    @Override
     public String getName() {
-        return mName;
+        return name;
     }
-    public String getBirthDate() {
-        return mBirthDate;
+
+    @Override
+    public String getBirthdate() {
+        return birthdate;
     }
+
+    @Override 
     public String getSex() {
-        return mSex;
+        return sex;
     }
 }
 
-class Student extends Person implements GradTeachingFellow{
-    private String add;
-    private String dept;
-
-    public Student(String name, String birthDate, String sex,String add, String dept){
-        super(name, birthDate, sex);
-        this.add = add;
-        this.dept = dept;
-    }
-    
-    public String getAddress(){
-        return add;
-    }
-    public String getDepartment(){
-        return dept;
-    }
-    public String getSupervisor() {
-        return Supervisor;
-    }
+interface Student {
+    String getDepartment();
+    String getAddress();
 }
 
-class Teacher extends Person implements GradTeachingFellow{
-    private int rank;
-    private String dept;
-    private String course;
+interface Teacher {
+    String getRank();
+    String getDepartment();
+    String getCourseInfo();
+}
 
-    public Teacher(String name, String birthDate, String sex,int rank, String dept, String course){
-        super(name, birthDate, sex);
-        this.rank = rank;
-        this.dept = dept;
-        this.course = course;
+class StudentClass extends PersonClass implements Student {
+
+    private String department;
+    private String address;
+
+    public StudentClass(String name, String birthdate, String sex, String department, String address) {
+        super(name, birthdate, sex);
+        this.department = department;
+        this.address = address;
     }
 
-    public int getRank() {
-        return rank;
-    }
+    @Override
     public String getDepartment() {
-        return dept;
+        return this.department;
     }
-    public String getCourseinfo() {
-        return course;
-    }
-    public String getSupervisor() {
-        return Supervisor;
+
+    @Override
+    public String getAddress() {
+        return this.address;
     }
 }
 
-interface GradTeachingFellow{
-    String Supervisor="Rudra";
-    String getSupervisor();
+
+class TeacherClass extends PersonClass implements Teacher {
+	
+    private String rank;
+    private String department;
+    private String courseInfo;
+
+    public TeacherClass(String name, String birthdate, String sex, String rank, String department, String courseInfo) {
+        super(name, birthdate, sex);
+        this.rank = rank;
+        this.department = department;
+        this.courseInfo = courseInfo;
+    }
+
+    @Override
+    public String getRank() {
+        return this.rank;
+    }
+
+    @Override
+    public String getDepartment() {
+        return this.department;
+    }
+
+    @Override
+    public String getCourseInfo() {
+        return this.courseInfo;
+    }
+}
+
+class GraduateTeachingFellow extends PersonClass implements Student, Teacher {
+
+    private String department;
+    private String address = "N.A";
+    private String rank = "N.A";
+    private String courseInfo = "N.A";
+
+    public GraduateTeachingFellow(StudentClass st) {
+        super(st.name, st.birthdate, st.sex);
+        this.department = st.getDepartment();
+        this.address = st.getAddress();
+    }
+
+    public GraduateTeachingFellow(TeacherClass tr) {
+        super(tr.name, tr.birthdate, tr.sex);
+        this.department = tr.getDepartment();
+        this.rank = tr.getRank();
+        this.courseInfo = tr.getCourseInfo();
+    }
+
+    @Override
+    public String getDepartment() {
+        return this.department;
+    }
+
+    @Override
+    public String getAddress() {
+        return this.address;
+    }
+
+    @Override
+    public String getRank() {
+        return this.rank;
+    }
+
+    @Override
+    public String getCourseInfo() {
+        return this.courseInfo;
+    }
+
+    public String getSupervisor() {
+        return "HOD";
+    }
 }
 
 public class Q14 {
-    public static void main(String[] args) {
-        Student s1 = new Student("Rudra Kashyap", "18-09-2004", "Male", "Raigarh","CSE" );
-        Teacher t1 = new Teacher("Asim", "5-06-1974", "Mala", 1, "CSE", "B. Tech");
-        System.out.println("\nStudent's details are..........");
-        System.out.println("Student's name = "+ s1.getName());
-        System.out.println("Student's birth date = "+ s1.getBirthDate());
-        System.out.println("Student's gender = "+ s1.getSex());
-        System.out.println("Student's address = "+ s1.getAddress());
-        System.out.println("Student's department  = "+ s1.getDepartment());
-        System.out.println("Student's supervisor = "+ s1.getSupervisor());
-        
-        System.out.println("\nFaculty details are.......");
-        System.out.println("Faculty's name = "+ t1.getName());
-        System.out.println("Faculty's birth date = "+ t1.getBirthDate());
-        System.out.println("Faculty's gender = "+ t1.getSex());
-        System.out.println("Faculty's rank = "+ t1.getRank());
-        System.out.println("Faculty's department  = "+ t1.getDepartment());
-        System.out.println("Faculty's course = "+ t1.getCourseinfo());
-        System.out.println("Faculty's supervisor = "+ t1.getSupervisor());
-    }
+	public static void main(String[] args) {
+		
+	StudentClass std = new StudentClass("Pintu Jain","01-09-2003","Male","CSE","Raigarh");
+	TeacherClass tec = new TeacherClass("Saroj Sir", "DD-MM-YYYY","Male","Sr. Associate Professor","CSE","OOPs using JAVA");
+		
+	GraduateTeachingFellow gtf1 = new GraduateTeachingFellow(std);
+	GraduateTeachingFellow gtf2 = new GraduateTeachingFellow(tec);
+		
+	System.out.println("Details of student are :-");
+        System.out.println("Name = " + std.getName());
+        System.out.println("Birth Date = " + std.getBirthdate());
+        System.out.println("Sex = " + std.getSex());
+        System.out.println("Address = " + std.getAddress());
+        System.out.println("Department = " + std.getDepartment());
+
+        System.out.println("\nDetails of teacher are :-");
+        System.out.println("Name = " + tec.getName());
+        System.out.println("Birth Date = " + tec.getBirthdate());
+        System.out.println("Sex = " + tec.getSex());
+        System.out.println("Rank = " + tec.getRank());
+        System.out.println("Department = " + tec.getDepartment());
+        System.out.println("Course = " + tec.getCourseInfo());
+
+        System.out.println("\n\nGraduate Teaching Fellow 1 Details are :- ");
+        System.out.println("Name = " + gtf1.getName());
+        System.out.println("Birth Date = " + gtf1.getBirthdate());
+        System.out.println("Sex = " + gtf1.getSex());
+        System.out.println("Rank = " + gtf1.getRank());
+        System.out.println("Address = " + gtf1.getAddress());
+        System.out.println("Department = " + gtf1.getDepartment());
+        System.out.println("Course = " + gtf1.getCourseInfo());
+
+        System.out.println("\n\nGraduate Teaching Fellow 2 Details are :- ");
+        System.out.println("Name = " + gtf2.getName());
+        System.out.println("Birth Date = " + gtf2.getBirthdate());
+        System.out.println("Sex = " + gtf2.getSex());
+        System.out.println("Address = " + gtf2.getAddress());
+        System.out.println("Rank = " + gtf2.getRank());
+        System.out.println("Department = " + gtf2.getDepartment());
+        System.out.println("Course = " + gtf2.getCourseInfo());
+	}
 }
