7381e6a69873ac556749034b1811197ae058da8b
Rudra Kashyap <rudrak04sh@gmail.com>
2023-10-12 22:20:40 +0530

    Choice automated

--- C/DSA/04092023.c
+++ C/DSA/04092023.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 struct queve{
     int val[5];
@@ -9,79 +8,78 @@ void display();
 void enqueve();
 void delqueve();
 void clearqueve();
-void choice();
 
 int main(){
     que.r=-1;    
     que.f=0;
-    choice();
-    display();
-}
-
-void choice(){
     int option;
-    printf("\nSelect option!\n");
-    printf("1 Empty\n");
-    printf("2 Enter Element\n");
-    printf("3 Delete Element\n");
-    printf("4 Display\n");
-    scanf("%d",&option);
-    switch (option)
-    {
-    case 1:
-        clearqueve();
-        break;
-    
-    case 2:
-        enqueve();
-        break;
-
-    case 3:
-        delqueve();
-        break;
-
-    case 4:
-        break;
-
-    default:
-        printf("Invalid option!\n");
-        choice();
-        break;
+    while(1){
+        printf("\nSelect option!\n");
+        printf("1 Empty\n");
+        printf("2 Enter Element\n");
+        printf("3 Delete Element\n");
+        printf("4 Display\n");
+        printf("5 Exit\n");
+        scanf("%d",&option);
+        switch (option)
+        {
+        case 1:
+            clearqueve();
+            break;
+        case 2:
+            enqueve();
+            break;
+        case 3:
+            delqueve();
+            break;
+        case 4:
+            display();
+            break;
+        case 5:
+            break;
+        default:
+            printf("Invalid option!\n");
+            break;
+        }
+        if(option==5){
+            return 0;
+        }
     }
 }
+
 void enqueve(){
     int x;
-    if (que.r>4){
+    if (que.r==4){
         printf("Queve Overloaded!\n");
     }else{
-        printf("Enter element\n==> ");
+        printf("Enter element:");
         scanf("%d",&x);
-        que.val[que.r++]=x;
+        que.val[++que.r]=x;
     }
-    choice();
 }
+
 void clearqueve(){
     que.r=-1;
     que.f=0;
     printf("Queve is Empty!\n");
-    choice();
 }
+
 void delqueve(){
-    if (que.r==0)
+    if (que.r==-1)
     {
         printf("Empty Queve\n");
     }else{
         ++que.f;
     }
-    choice();
 }
+
 void display(){
-    if (que.r==0){
+    if (que.r==-1){
         printf("Queue is Empty");
     }
     else{
         printf("Elements on queue are: ");
-        for(int i=0;i<que.r;i++){
+        for(int i=0;i<=que.r;i++){
             printf("%d ",que.val[i]);
         }
     }
