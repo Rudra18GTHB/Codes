a038d93ec7fda1496fb032585a185b70dc25a3a2
Rudra Kashyap <rudrak04sh@gmail.com>
2023-10-19 10:47:10 +0530

    Removed Extra Variables

--- C/DSA/bubble.c
+++ C/DSA/bubble.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int data[]={32,71,12,45,26},i,step,n=5,temp;
+    int data[]={32,71,12,45,26},i,step,n=5;
     for(step = 0; step < n-1; ++step){
         for(i=0; i<n-step-1; ++i){
             if(data[i]>data[i+1]){
-                temp=data[i];
-                data[i]=data[i+1];
-                data[i+1]=temp;
+                data[i]+=data[i+1];
+                data[i+1]=data[i]-data[i+1];
+                data[i]-=data[i+1];
             }
         }
     }
