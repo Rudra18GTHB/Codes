5c58e221eaca410ac919a9b9a4e36bb56290c038
Rudra18GTHB <rudrak04sh@gmail.com>
2023-12-09 22:49:53 +0530

    Completed: Question 5

--- Java/Assignment/Q5.java
+++ Java/Assignment/Q5.java
@@ -21,6 +21,31 @@ class ArrayGenerator extends Thread{
     }
 }
 
+class SearchElement extends Thread{
+    private int[] arr;
+    private int num;
+    private boolean found;
+
+    SearchElement(int[] arr, int num){
+        this.arr =arr;
+        this.num=num;
+        this.found=false;
+    }
+
+    @Override
+    public void run(){
+        for (int i = 0; i < arr.length; i++) {
+            if(num==arr[i]){
+                found= true;
+                break;
+            }
+        }
+    }
+    public boolean getFound(){
+        return found;
+    }
+}
+
 public class Q5 {
     public static void main(String[] args) {
         ArrayGenerator arr = new ArrayGenerator(500);
@@ -38,8 +63,32 @@ public class Q5 {
         int number = scan.nextInt();
 
         int midpoint = array.length/2;
-        int[] firsthalf = new int[midpoint];
 
-        
+        int[] arr1 = new int[midpoint];
+        int[] arr2 = new int[array.length-midpoint];
+
+        System.arraycopy(array, 0, arr1, 0, midpoint);
+        System.arraycopy(array, midpoint, arr2, 0 , array.length-midpoint);
+
+        SearchElement F1 = new SearchElement(arr1, number);
+        SearchElement F2 = new SearchElement(arr2, number);
+
+        F1.start();
+        F2.start();
+
+        try {
+            F1.join();
+            F2.join();
+        } catch (InterruptedException e) {
+            e.getStackTrace();
+        }
+
+
+        if(F1.getFound() || F2.getFound()){
+            System.out.println("Success");
+        }
+        else{
+            System.out.println("Beter Luck Next Time");
+        }
     }
 }
