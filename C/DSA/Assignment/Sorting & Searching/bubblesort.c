5a657458fb26c29289f7b83c5b09ea18d100d03d
Rudra18GTHB <rudrak04sh@gmail.com>
2023-12-16 23:43:30 +0530

    Minor Modification

--- C/DSA/Assignment/Sorting & Searching/bubblesort.c
+++ C/DSA/Assignment/Sorting & Searching/bubblesort.c
@@ -2,8 +2,8 @@
 int main()
 {
     int data[]={32,71,12,45,26},i,step,n=5;
-    for(step = 0; step < n-1; ++step){
-        for(i=0; i<n-step-1; ++i){
+    for(step = 0; step < n-1; step++){
+        for(i=0; i<n-step-1; i++){
             if(data[i]>data[i+1]){
                 data[i]+=data[i+1];
                 data[i+1]=data[i]-data[i+1];
