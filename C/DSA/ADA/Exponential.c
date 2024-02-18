440b388c4c5133a0e1aeb933bb0ce5e06ea1d1aa
rudraa-ksh <rudrak04sh@gmail.com>
2024-02-18 22:30:09 +0530

    Experiment 7 Part 2

--- C/DSA/ADA/Exponential.c
+++ C/DSA/ADA/Exponential.c
@@ -1,15 +1,37 @@
 #include <stdio.h>
 
+// int main()
+// {
+//     int base, expo, final;
+//     printf("Enter the base number: ");
+//     scanf("%d",&base);
+//     printf("Enter the exponent: ");
+//     scanf("%d",&expo);
+//     for(int i=0;i<expo;i++){
+//         final = base*base;
+//     }
+//     printf("Computed Number: %d",final);
+//     return 0;
+// }
+
 int main()
 {
-    int base, expo, final;
+    int base, expo;
     printf("Enter the base number: ");
     scanf("%d",&base);
     printf("Enter the exponent: ");
     scanf("%d",&expo);
-    for(int i=0;i<expo;i++){
-        final = base*base;
+    int m=expo;
+    int power=1;
+    int z = base;
+    while(m>0){
+        while (m%2==0){
+            m=m/2;
+            z=z*z;
+        }
+        m = m-1;
+        power = power *z;
     }
-    printf("Computed Number: %d",final);
+    printf("%d",power);
     return 0;
 }
